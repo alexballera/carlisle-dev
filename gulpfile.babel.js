086648da5fea61991152ad513cdf8c6b8ac4543f
Alex Ballera <alexballera@gmail.com>
2017-01-18 15:26:45 -0400

    Instalando npm y dependencias

---
+++ gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp'
+require('require-dir')('./tasks')
+
+/* Instalar las siguientes dependencias:
+npm i -S pug yo-yo page empty-element moment intl intl-relativeformat superagent axios multer file-extension
+npm i -D gulp babel-preset-latest babelify browser-sync browserify del gulp-autoprefixer gulp-cache gulp-cssnano gulp-gh-pages gulp-htmlmin gulp-imagemin gulp-inject gulp-notify gulp-plumber gulp-rename gulp-sass gulp-sass-glob gulp-uglify gulp-uncss imagemin-jpegtran imagemin-optipng imagemin-pngquant imagemin-svgo jquery require-dir vinyl-buffer vinyl-source-stream babel-plugin-transform-regenerator
+
+*/
+// Build
+gulp.task('build', ['copy', 'inject'], () => {
+  // gulp.start('styles', 'scripts', 'images')
+  gulp.start('build:html', 'styles', 'scripts', 'images')
+})
+
+// Default
+gulp.task('default', ['clean'], () => {
+  gulp.start('serve', 'watch', 'build')
+  // gulp.start('watch', 'build')
+})
---
+++ tasks/scripts.js
@@ -0,0 +1,28 @@
+import gulp from 'gulp'
+import browserify from 'browserify'
+import source from 'vinyl-source-stream'
+import buffer from 'vinyl-buffer'
+import uglify from 'gulp-uglify'
+import babelify from 'babelify'
+import rename from 'gulp-rename'
+
+// Agregado presets y plugins en .babelrc o en package.json plugin: "transform-regenerator"
+
+gulp.task('scripts', () => {
+  return browserify('./src/scripts/main.js')
+  .transform(babelify)
+  .bundle()
+  .on('error', (err) => {
+    console.log(err)
+    this.emit('end')
+  })
+  .pipe(source('main.js'))
+  .pipe(buffer())
+  .pipe(rename('index.js'))
+  .pipe(gulp.dest('./build/scripts'))
+  .pipe(uglify())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('./src/scripts'))
+  .pipe(gulp.dest('./build/scripts'))
+  .pipe(gulp.dest('./public/scripts'))
+})
