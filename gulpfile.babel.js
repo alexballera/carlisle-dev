7faa0d1bccaccaa343ebd29c98324221a06ba4e4
Alex Ballera <alexballera@gmail.com>
2017-01-19 15:17:14 -0400

    Modularizando header

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,14 @@ import gulp from 'gulp'
 require('require-dir')('./tasks')
 
 /* Instalar las siguientes dependencias:
-npm i -S pug yo-yo page empty-element moment intl intl-relativeformat superagent axios multer file-extension
-npm i -D sass-lint gulp-sass-lint gulp babel-preset-latest babelify browser-sync browserify del gulp-autoprefixer gulp-cache gulp-cssnano gulp-gh-pages gulp-htmlmin gulp-imagemin gulp-inject gulp-notify gulp-plumber gulp-rename gulp-sass gulp-sass-glob gulp-uglify gulp-uncss imagemin-jpegtran imagemin-optipng imagemin-pngquant imagemin-svgo jquery require-dir vinyl-buffer vinyl-source-stream babel-plugin-transform-regenerator
-
+npm i -S babel-eslint bootstrap standard yo-yo empty-element
+npm i -D babel-plugin-transform-regenerator babel-preset-latest babelify browser-sync
+browserify del font-awesome gulp gulp-autoprefixer gulp-cache gulp-cssnano gulp-gh-pages
+gulp-htmlmin gulp-imagemin gulp-inject gulp-notify gulp-plumber gulp-rename gulp-sass
+gulp-sass-glob gulp-sass-lint gulp-uglify gulp-uncss imagemin-jpegtran imagemin-optipng
+imagemin-pngquant imagemin-svgo jquery require-dir sass-lint vinyl-buffer vinyl-source-stream
 */
+
 // Build
 gulp.task('build', ['copy', 'inject'], () => {
   // gulp.start('styles', 'scripts', 'images')
--- src/header/index.js
+++ src/header/index.js
@@ -1,5 +1,40 @@
 'use strict'
 
+import yo from 'yo-yo'
+import empty from 'empty-element'
+
+var header = document.getElementById('header')
+
+var nav = yo`
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded center">
+  <button class="navbar-toggler navbar-toggler-right navbar-button" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span>Menu </span> <i class="fa fa-bars" aria-hidden="true"></i>
+  </button>
+  <a class="navbar-brand" href="#">
+    <img src="./images/logo.png" alt="">
+  </a>
+  <div class="collapse navbar-collapse justify-content-end navbar-top" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link align-middle" href="#">About Us <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Team</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Our Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Carrers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+`
+
 export default () => {
-  console.log('estoy aqui')
+  empty(header).appendChild(nav)
 }
