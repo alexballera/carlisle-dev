44261010283994e4464a5e936afa6e3c1f866074
Alex Ballera <alexballera@gmail.com>
2017-01-22 11:59:52 -0400

    Modulado de home footer

--- src/homepage/templates/footer.js
+++ src/homepage/templates/footer.js
@@ -1,3 +1,30 @@
+'use strict'
+
+import yo from 'yo-yo'
+import empty from 'empty-element'
+
+var footer = document.getElementById('home-footer')
+
+var el = yo `
+<div class="row no-gutters home-footer-container">
+    <div class="col-12 home-footer-banner">
+        <div class="home-footer-opacity"></div>
+        <div class="home-footer-contenido row no-gutters justify-content-center align-items-center">
+            <h3 class="col-10 col-md-9 col-xl-11 home-footer-title">
+                Carlisle is a leading, highly diversified
+                global investment management firm
+            </h3>
+            <p class="col-10 col-md-9 col-xl-11 home-footer-content">
+                Carlisle applies its deep experience and specialized expertise
+                across a range of investment strategies -private equity,
+                credit, liquid markets and traditional fixed income - 
+                on behalf of investors and private clients wordwide.
+            </p>
+        </div>
+    </div>
+</div>
+`
+
 export default () => {
-  console.log('Estoy en home footer')
+  empty(footer).appendChild(el)
 }
