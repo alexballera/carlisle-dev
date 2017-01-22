1f48dc6992f82cb72ae38b4359baef15c50f657c
Alex Ballera <alexballera@gmail.com>
2017-01-22 10:18:49 -0400

    Modulado study

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,9 +3,11 @@
 import header from './templates/header'
 import opportunities from './templates/opportunities'
 import team from './templates/team'
+import study from './templates/study'
 
 export default () => {
   header()
   opportunities()
   team()
+  study()
 }
