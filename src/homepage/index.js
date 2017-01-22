0cd5c9eb860187810202ef615d5a133fb1330ca4
Alex Ballera <alexballera@gmail.com>
2017-01-22 11:54:19 -0400

    Terminado home footer

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -4,10 +4,12 @@ import header from './templates/header'
 import opportunities from './templates/opportunities'
 import team from './templates/team'
 import study from './templates/study'
+import footer from './templates/footer'
 
 export default () => {
   header()
   opportunities()
   team()
   study()
+  footer()
 }
---
+++ src/homepage/templates/footer.js
@@ -0,0 +1,3 @@
+export default () => {
+  console.log('Estoy en home footer')
+}
