7fcef253c2804d192d39dc4286c5260c9d95b0f7
Alex Ballera <alexballera@gmail.com>
2017-01-23 20:18:52 -0400

    Moduladizado de picture

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,40 +1,23 @@
 'user strict'
 
 import yo from 'yo-yo'
+import picture from '../picture-about'
 
 module.exports = {
   template: yo`
   <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner" role="listbox">
       <div class="carousel-item active content-opportunities row no-gutters">
-        <picture class="content-picture content-picture-left col-6">
-            <img class="d-block img-fluid" src="http://lorempixel.com/400/400/city/" alt="" class="content-picture-img">
-            <div class="content-picture-contenido row align-items-center no-gutters">
-            <h2 class="content-picture-title col-12">
-                Why Carlisle?
-            </h2>
-            <p class="content-picture-parrafo col-12">
-                Our experience gives us the insight and patience to identify opportunities with consistence absolute returns.
-            </p>
-            <a href="#" class="content-picture-boton col-8">
-                Get in touch
-            </a>
-            </div>
-        </picture>
-        <picture class="content-picture content-picture-rigth col-6">
-          <img class="d-block img-fluid" src="http://lorempixel.com/400/400/sports/" alt="" class="content-picture-img">
-          <div class="content-picture-contenido row align-items-center no-gutters">
-          <h2 class="content-picture-title col-12">
-              Our difference
-          </h2>
-          <p class="content-picture-parrafo col-12">
-              Carlisle is a financial structuring and consulting firm specializing in alternative asset classes.
-          </p>
-          <a href="#" class="content-picture-boton col-8">
-              Get in touch
-          </a>
-          </div>
-        </picture>
+        ${picture.why}
+        ${picture.difference}
+      </div>
+      <div class="carousel-item content-opportunities row no-gutters">
+        ${picture.community}
+        ${picture.why2}
+      </div>
+      <div class="carousel-item content-opportunities row no-gutters">
+        ${picture.difference2}
+        ${picture.community2}
       </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -47,14 +30,3 @@ module.exports = {
     </a>
   </div>`
 }
-
-/*
-<div class="carousel-item">
-  <img class="d-block img-fluid" src="http://lorempixel.com/400/400/sports/" alt="Second slide">
-  <img class="d-block img-fluid" src="http://lorempixel.com/400/400/food/" alt="Second slide">
-</div>
-<div class="carousel-item">
-  <img class="d-block img-fluid" src="http://lorempixel.com/400/400/people/" alt="Second slide">
-  <img class="d-block img-fluid" src="http://lorempixel.com/400/400/city/" alt="Second slide">
-</div>
-*/
---
+++ src/components/picture-about/index.js
@@ -0,0 +1,100 @@
+'use strict'
+
+import yo from 'yo-yo'
+
+module.exports = {
+  why: yo`
+  <picture class="content-picture content-picture-left col-6">
+      <img class="d-block img-fluid" src="http://lorempixel.com/400/400/city/" alt="" class="content-picture-img">
+      <div class="content-picture-contenido row align-items-center no-gutters">
+      <h2 class="content-picture-title col-12">
+          Why Carlisle?
+      </h2>
+      <p class="content-picture-parrafo col-12">
+          Our experience gives us the insight and patience to identify opportunities with consistence absolute returns.
+      </p>
+      <a href="#" class="content-picture-boton col-8">
+          Get in touch
+      </a>
+      </div>
+  </picture>`,
+  difference: yo`
+  <picture class="content-picture content-picture-rigth col-6">
+    <img class="d-block img-fluid" src="http://lorempixel.com/400/400/sports/" alt="" class="content-picture-img">
+    <div class="content-picture-contenido row align-items-center no-gutters">
+    <h2 class="content-picture-title col-12">
+        Our difference
+    </h2>
+    <p class="content-picture-parrafo col-12">
+        Carlisle is a financial structuring and consulting firm specializing in alternative asset classes.
+    </p>
+    <a href="#" class="content-picture-boton col-8">
+        Get in touch
+    </a>
+    </div>
+  </picture>`,
+  community: yo`
+  <picture class="content-picture content-picture-rigth col-6">
+    <img class="d-block img-fluid" src="http://lorempixel.com/400/400/sports/" alt="" class="content-picture-img">
+    <div class="content-picture-contenido row align-items-center no-gutters">
+    <h2 class="content-picture-title col-12">
+        Community Reach
+    </h2>
+    <p class="content-picture-parrafo col-12">
+        Street Soccer USA (SSUSA) is a 501(c)(3) non-profit organization
+        that uses sports to help adults and youths (ages 15+) get out of
+        homelessness. By creating a team
+    </p>
+    <a href="#" class="content-picture-boton col-8">
+        Get in touch
+    </a>
+    </div>
+  </picture>`,
+  why2: yo`
+  <picture class="content-picture content-picture-left col-6">
+      <img class="d-block img-fluid" src="http://lorempixel.com/400/400/city/" alt="" class="content-picture-img">
+      <div class="content-picture-contenido row align-items-center no-gutters">
+      <h2 class="content-picture-title col-12">
+          Why Carlisle?
+      </h2>
+      <p class="content-picture-parrafo col-12">
+          Our experience gives us the insight and patience to identify opportunities with consistence absolute returns.
+      </p>
+      <a href="#" class="content-picture-boton col-8">
+          Get in touch
+      </a>
+      </div>
+  </picture>`,
+  difference2: yo`
+  <picture class="content-picture content-picture-rigth col-6">
+    <img class="d-block img-fluid" src="http://lorempixel.com/400/400/sports/" alt="" class="content-picture-img">
+    <div class="content-picture-contenido row align-items-center no-gutters">
+    <h2 class="content-picture-title col-12">
+        Our difference
+    </h2>
+    <p class="content-picture-parrafo col-12">
+        Carlisle is a financial structuring and consulting firm specializing in alternative asset classes.
+    </p>
+    <a href="#" class="content-picture-boton col-8">
+        Get in touch
+    </a>
+    </div>
+  </picture>`,
+  community2: yo`
+  <picture class="content-picture content-picture-rigth col-6">
+    <img class="d-block img-fluid" src="http://lorempixel.com/400/400/sports/" alt="" class="content-picture-img">
+    <div class="content-picture-contenido row align-items-center no-gutters">
+    <h2 class="content-picture-title col-12">
+        Community Reach
+    </h2>
+    <p class="content-picture-parrafo col-12">
+        Street Soccer USA (SSUSA) is a 501(c)(3) non-profit organization
+        that uses sports to help adults and youths (ages 15+) get out of
+        homelessness. By creating a team
+    </p>
+    <a href="#" class="content-picture-boton col-8">
+        Get in touch
+    </a>
+    </div>
+  </picture>`
+}
