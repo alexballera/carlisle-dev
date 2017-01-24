7ba4d9605ed4d0f4b07f35f8cd33fa48f56c404c
Alex Ballera <alexballera@gmail.com>
2017-01-24 18:17:02 -0400

    Optimizando gulpfile

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,11 +1,19 @@
 import gulp from 'gulp'
 import inject from 'gulp-inject'
+import series from 'stream-series'
 
 gulp.task('inject', () => {
+  var vendorJS = gulp.src(['./src/js/vendors/*.js'])
+  var appJS = gulp.src(['./src/js/index.min.js'])
+  var vendorCSS = gulp.src(['./src/js/vendors/*.*.ccs'])
+  var appCSS = gulp.src(['./src/styles/style.min.css'])
+
   gulp.src('./src/index.html')
-    .pipe(inject(gulp.src([
-      './src/styles/style.min.css',
-      './src/scripts/index.min.js'
+    .pipe(inject(series([
+      vendorJS,
+      appJS,
+      vendorCSS,
+      appCSS
     ],
       {
         read: false
