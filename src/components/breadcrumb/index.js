3be768dc2c515041282836c65707780568edad58
Alex Ballera <alexballera@gmail.com>
2017-01-25 23:29:04 -0400

    Agregada funcionalidad al link home de breadcrumb

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -4,12 +4,12 @@ import yo from 'yo-yo'
 
 module.exports = {
   template: yo`
-  <ol class="breadcrumb col-10 offset-2">
-    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link"></a></li>
-    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link"></a></li>
-    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link"></a></li>
-    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link"></a></li>
-    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link"></a></li>
-    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link"></a></li>
+  <ol class="breadcrumb col-10 offset-2" id="breadcrumb">
+    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link" id="breadcrumb-link-1"></a></li>
+    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link" id="breadcrumb-link-2"></a></li>
+    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link" id="breadcrumb-link-3"></a></li>
+    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link" id="breadcrumb-link-4"></a></li>
+    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link" id="breadcrumb-link-5"></a></li>
+    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link" id="breadcrumb-link-6"></a></li>
   </ol>`
 }
--- src/lib/navigation.js
+++ src/lib/navigation.js
@@ -6,12 +6,15 @@ export default () => {
   // var mediaQuery = window.matchMedia('screen and (min-width: 998px)')
   // var mq = mediaQuery.matches
   var main = $('#main')
+
   var topNavbar = $('#topNavbar')
   var navbarNav = topNavbar.find('#navbarNav')
   var linkHome = topNavbar.find('#linkHome')
   var linkAbout = topNavbar.find('a#linkAbout')
   var about = main.find('#about')
   var home = main.find('#home')
+
+  var breadcrumb = $('#breadcrumb')
 
   // mediaQuery.addListener()
 
@@ -32,12 +35,21 @@ export default () => {
     // navbarNav.removeClass('show')
   }
 
+  // Top Navbar
+
   linkAbout.on('click', (e) => {
     e.preventDefault()
     showAbout()
   })
 
   linkHome.on('click', (e) => {
+    e.preventDefault()
+    showHome()
+  })
+
+  // Breadcrumb
+
+  breadcrumb.find('#breadcrumb-link-1').on('click', (e) => {
     e.preventDefault()
     showHome()
   })
