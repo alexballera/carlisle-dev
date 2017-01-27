447f7121caaff85526dc55fd5c376bd4777aee08
Alex Ballera <alexballera@gmail.com>
2017-01-27 12:48:07 -0400

    Corregido carousel

--- src/homepage/templates/pictures-about/index.js
+++ src/homepage/templates/pictures-about/index.js
@@ -19,7 +19,7 @@ module.exports = {
           </div>
       </picture>`,
   why2: yo`
-      <picture class="content-picture">
+      <picture class="content-picture col-md-5">
           <img class="d-block img-fluid" src="" alt="" id="home-why2">
           <div class="content-picture-contenido row align-items-center no-gutters">
           <h2 class="content-picture-title col-12">
@@ -48,8 +48,55 @@ module.exports = {
           </a>
           </div>
       </picture>`,
+  difference2: yo`
+      <picture class="content-picture col-md-5">
+          <img class="d-block img-fluid" src="" alt="" id="home-difference-img">
+          <div class="content-picture-contenido row align-items-center no-gutters">
+          <h2 class="content-picture-title col-12">
+              Our difference
+          </h2>
+          <p class="content-picture-parrafo col-12">
+              Carlisle is a financial structuring and consulting firm specializing in alternative asset classes.
+          </p>
+          <a href="#" class="content-picture-boton col-8">
+              Get in touch
+          </a>
+          </div>
+      </picture>`,
+  difference3: yo`
+      <picture class="content-picture col-md-5">
+          <img class="d-block img-fluid" src="" alt="" id="home-difference-img">
+          <div class="content-picture-contenido row align-items-center no-gutters">
+          <h2 class="content-picture-title col-12">
+              Our difference
+          </h2>
+          <p class="content-picture-parrafo col-12">
+              Carlisle is a financial structuring and consulting firm specializing in alternative asset classes.
+          </p>
+          <a href="#" class="content-picture-boton col-8">
+              Get in touch
+          </a>
+          </div>
+      </picture>`,
   community: yo`
       <picture class="content-picture" id="home-community">
+          <img class="d-block img-fluid" src="" alt="" id="home-community-img">
+          <div class="content-picture-contenido row align-items-center no-gutters">
+          <h2 class="content-picture-title col-12">
+              Community Reach
+          </h2>
+          <p class="content-picture-parrafo col-12">
+              Street Soccer USA (SSUSA) is a 501(c)(3) non-profit organization
+              that uses sports to help adults and youths (ages 15+) get out of
+              homelessness. By creating a team
+          </p>
+          <a href="#" class="content-picture-boton col-8">
+              Get in touch
+          </a>
+          </div>
+      </picture>`,
+  community2: yo`
+      <picture class="content-picture col-md-5">
           <img class="d-block img-fluid" src="" alt="" id="home-community-img">
           <div class="content-picture-contenido row align-items-center no-gutters">
           <h2 class="content-picture-title col-12">
